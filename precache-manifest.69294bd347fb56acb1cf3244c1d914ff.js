ea3438911dbd753c911b2ae39cd51fdaf1264d4c
Code Sandbox <hello@codesandbox.io>
2021-05-17 14:31:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.69294bd347fb56acb1cf3244c1d914ff.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb82a71b7d450be154d36eb73c69128",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/index.html"
+  },
+  {
+    "revision": "27dfe83c6cb28983c88c",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/css/main.e0dc895b.chunk.css"
+  },
+  {
+    "revision": "8ea9e115f0c949c79fab",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/js/2.0ae509f9.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/js/2.0ae509f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27dfe83c6cb28983c88c",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/js/main.267b7364.chunk.js"
+  },
+  {
+    "revision": "c15c530f08e11d20fb80",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/js/runtime-main.929642d3.js"
+  },
+  {
+    "revision": "ec53b1c86834aff53b86d3db6bc3a55a",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/amazed.ec53b1c8.svg"
+  },
+  {
+    "revision": "24cb2abfd81580fd5e9b99292af9e718",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/care.24cb2abf.svg"
+  },
+  {
+    "revision": "90cb742026e85881fcc7464f6eaf041c",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/feed1.90cb7420.png"
+  },
+  {
+    "revision": "8dec8a50f5d43c2974e1012ca2ab794d",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/friend1.8dec8a50.jpg"
+  },
+  {
+    "revision": "ef2a9aaa1e2552a6ccce9b66c1b723cd",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/friend2.ef2a9aaa.jpg"
+  },
+  {
+    "revision": "e2862a26a227b907764c094f3dd8202e",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/friend4.e2862a26.jpg"
+  },
+  {
+    "revision": "751397166a05cfc7e1c9ea728f372451",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/friend6.75139716.jpg"
+  },
+  {
+    "revision": "3470b0bc1556961892de4f9fe0bcd7cd",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/friend7.3470b0bc.jpg"
+  },
+  {
+    "revision": "3f581f4d5d368194b95e9b76cee24fce",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/friend9.3f581f4d.jpg"
+  },
+  {
+    "revision": "c4591bf5c5c1b82726c324bb6b60ca49",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/games.c4591bf5.png"
+  },
+  {
+    "revision": "1340f6cbe67d9c64707b2eec601c91f3",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/heart.1340f6cb.svg"
+  },
+  {
+    "revision": "3388777c70b640fd225336399dfb6b86",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/laugh.3388777c.svg"
+  },
+  {
+    "revision": "030d77c4bf41eef0dd34c9d99a22586d",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/like.030d77c4.svg"
+  },
+  {
+    "revision": "791420f2b0855ea5a70687819496e9b2",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/sad.791420f2.svg"
+  },
+  {
+    "revision": "034fe735355473c9eef1179a71e24fef",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/sponsored1.034fe735.jpg"
+  },
+  {
+    "revision": "d6f8f72113f34563efabdb001550f234",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/sponsored2.d6f8f721.jpg"
+  },
+  {
+    "revision": "258aa67187be50bdca921863f95b05ba",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story1.258aa671.jpg"
+  },
+  {
+    "revision": "22f9e1b1b6e176e9f42808abddfbbe6d",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story2.22f9e1b1.jpg"
+  },
+  {
+    "revision": "5fc8ea556f9b72367c80db12032e3e70",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story3.5fc8ea55.jpg"
+  },
+  {
+    "revision": "0e460c96bca845c111e65a1557a90b75",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story4.0e460c96.jpg"
+  },
+  {
+    "revision": "56b2d15de15067cf7ac8132f1669743e",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story5.56b2d15d.jpg"
+  },
+  {
+    "revision": "5fc8ea556f9b72367c80db12032e3e70",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story6.5fc8ea55.jpg"
+  },
+  {
+    "revision": "0e460c96bca845c111e65a1557a90b75",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story7.0e460c96.jpg"
+  },
+  {
+    "revision": "56b2d15de15067cf7ac8132f1669743e",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/story8.56b2d15d.jpg"
+  },
+  {
+    "revision": "24cb2abfd81580fd5e9b99292af9e718",
+    "url": "https://dougzybuydesign.github.io/csb-su0ko/static/media/think.24cb2abf.svg"
+  }
+]);
